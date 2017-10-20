aa4d3715444c42c14478bc586e08ae22fe094cdf
weshebert20 <weshebert20@gmail.com>
2017-10-20 10:49:39 -0600

    issues with api

--- server.js
+++ server.js
@@ -26,19 +26,26 @@ app.set('views', __dirname);
 app.engine('ejs', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
-// connect to db models
+//connect to db models//
 var db = require('./models');
 
-// serve static files in public
+//serve static files in public//
 app.use(express.static('public'));
-
 
 
 //////////////
 ////ROUTES////
 //////////////
 
+//route to log-in page
+app.get('/', function(req, res){
+	res.render('./views/index');
+});
 
+
+///////////
+//KEY VAR//
+///////////
 
 var options = {
     url : "https://developers.zomato.com/api/v2.1/search?entity_id=305&count=10&lat=39.7344&lon=-104.9726",
@@ -46,7 +53,7 @@ var options = {
     gzip:true
   };
 
-
+//function that gets name/rating/photo
 var find = function(object){
 	if (object.length === 0){
 		return("Sorry");
@@ -63,27 +70,31 @@ var find = function(object){
 	}
 };
 
+//home page
 app.get("/results", function(req, res){
 	res.render('./views/home');
 });
 
+//results search page
 app.get("/resultsSearch", function(req, res){
 	request(options, function(err, response, body){
 		if(!err && response.statusCode === 200) {
+			//parses body
 			var respObj = JSON.parse(body);
+			//returns restaurants in body
 			var restaurants = respObj.restaurants;
+			//returns find function (name,rating,photo)
 			var found = find(restaurants);
-			res.send(found);
-
+			console.log(found);
+			//renders on page
+			res.render('./views/resultsSearch', {found});
 		} else {
 			console.log(error);
 		}
 	});
 });
 
-app.get('/', function(req, res){
-	res.render('./views/index');
-});
+
 
 
 
