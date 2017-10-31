6cff97ba04ddf94c94b0528cfff3af862e49290a
weshebert20 <weshebert20@gmail.com>
2017-10-30 21:51:07 -0600

    heroku

--- public/script.js
+++ public/script.js
@@ -13,7 +13,7 @@ $( document ).ready(function() {
 		$.ajax({	
 			async: true,		
 			type:'post',
-			url:'/resultsSearch',
+			url:'/results',
 			data: {nameURL:nameURL},
 			success: function(homeSearch){
 				$('#bodySecond').html(homeSearch);
