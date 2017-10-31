7527f37d4167ae8db76948963484818cefeef002
weshebert20 <weshebert20@gmail.com>
2017-10-30 23:40:06 -0600

    heroku

--- models/index.js
+++ models/index.js
@@ -1,10 +1,9 @@
 
 var mongoose = require("mongoose");
-var dbURI = 'mongodb://locationToMyMongoDB';
-
-if (process.env.NODE_ENV === 'production') {
-    dbURI = process.env.MONGOLAB_URI;
-}
+mongoose.connect( process.env.MONGODB_URI || 
+                  process.env.MONGOLAB_URI || 
+                  process.env.MONGOHQ_URL || 
+                  "mongodb://localhost/happy_hour");
 
 // module.exports.Restaurant = require("./user.js");
 module.exports.Restaurant = require("./rest.js");
