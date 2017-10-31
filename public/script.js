e3745aa97750a7cb694d2de5fa2d5b0e1c0923ca
weshebert20 <weshebert20@gmail.com>
2017-10-30 23:03:47 -0600

    heroku

--- public/script.js
+++ public/script.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
 		var name = $('#inputName').val();
 		
 		// add name searched value to URL
-		var nameURL = 'http://developers.zomato.com/api/v2.1/search?q=' + name + '&count=8&sort=rating';
+		var nameURL = 'https://developers.zomato.com/api/v2.1/search?q=' + name + '&count=8&sort=rating';
 		console.log(nameURL);
 		$.ajax({	
 			async: true,		
