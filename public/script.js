6ae635c14e5462193d15b5d5b2ec5f56430beaf1
weshebert20 <weshebert20@gmail.com>
2017-10-30 19:26:16 -0600

    heroku

--- public/script.js
+++ public/script.js
@@ -13,7 +13,7 @@ $( document ).ready(function() {
 		$.ajax({	
 			async: true,		
 			type:'post',
-			url:'https://damp-brook-42509.herokuapp.com/results#',
+			url:'/results#',
 			data: {nameURL:nameURL},
 			success: function(homeSearch){
 				$('#bodySecond').html(homeSearch);
