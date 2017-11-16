08081566a09f00818f5834c269702aef4c8d3693
weshebert20 <weshebert20@gmail.com>
2017-11-16 16:54:35 -0700

    heroku

--- public/script.js
+++ public/script.js
@@ -323,7 +323,7 @@ $( document ).ready(function() {
 	});
 
 	$('#signOut').click(function() {
-    document.location.href = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=https://damp-brook-42509.herokuapp.com/";
+    document.location.href = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=https://damp-brook-42509.herokuapp.com";
 });
 
 
